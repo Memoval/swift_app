c67c0c8bc574404c938e8d49c00ba2eaf589145a
Guillermo Valdivia <g.valdivia10@gmail.com>
2018-02-19 20:25:11 -0500

    My first commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,10 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var The_title: UILabel!
+    
+    var tap = 0
+    
+    @IBAction func The_Button_pushed(_ sender: Any) {
+         The_title.text = "Hello there MOFAKA!"
+        tap = tap + 1
+        if tap > 5 {
+            The_title.text="Stop pusing that shit mofaka!"
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor.black
+       
     }
 
     override func didReceiveMemoryWarning() {
